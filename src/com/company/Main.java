a12102ce0e4b0489820c7a481d63107b040ef179
TrungDucMai <trungmaiduc196@gmail.com>
2021-07-10 18:18:48 +0700

    taomoi

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setClass("CO3");
+        student.setName("Trung");
+    }
+}
---
+++ src/com/company/Student.java
@@ -0,0 +1,16 @@
+
+package com.company;
+public class Student {
+    private String Name = "John";
+    private String Class = "CO2";
+
+    public Student (){
+
+    }
+    protected void setName(String Name1){
+        Name = Name1;
+    }
+     void setClass (String class1){
+        Class = class1;
+    }
+}
